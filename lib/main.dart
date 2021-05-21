4afe0a446bd1383006035ac9654445d91dc357e4
dikdudu <dikdudu@hotmail.com>
2021-05-21 11:09:34 -0300

    finalizando config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cuidapet/app/app_module.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp();
 
   runApp(ModularApp(module: AppModule()));
 }
